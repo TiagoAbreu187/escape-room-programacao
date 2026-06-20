aa5fe9915689e77d4af3a1e0631f66174bdfbb22
Tiago Abreu <tiagoabreu.carv@gmail.com>
2026-06-20 17:32:12 -0300

    Update2 Jogo.py
    Foi adicionado contagem de tentativas

--- Jogo.py
+++ Jogo.py
@@ -2,24 +2,27 @@ import random
 
 def game():
     numero = random.randint(1,100)
+    tentativas = 0
     # print(numero)
     print("Um número entre 1 a 100 foi sorteado! Adivinhe!")
     
     while True:
         try:
-            escolha = int(input("Escreva o número: "))
+            escolha = int(input("Escreva o número:"))
         except ValueError:
             print("Digite apenas números!")
             continue
-            
+        
         if escolha < 1 or escolha > 100:
             print("Escolha um número entre 1 a 100!")    
-        elif numero > escolha:
+
+        tentativas = tentativas + 1
+        if numero > escolha:
             print("Seu número é menor que o número sorteado")
         elif numero < escolha:
             print("Seu número é maior que o número sorteado")
         else:
-            print("Você acertou!")
+            print("Você acertou em",tentativas,"tentativas!")
             break
 
 game()
