602d614bb406a589dba383e992579bd922fd868e
Tiago Abreu <tiagoabreu.carv@gmail.com>
2026-06-20 15:38:33 -0300

    Update Jogo.py
    Foi adicionado uma função para evitar que alguém escrevesse uma letra em vez de um número.  E foram feitas melhorias na parte das condições.

--- Jogo.py
+++ Jogo.py
@@ -1,31 +1,25 @@
 import random
 
-
 def game():
-    
     numero = random.randint(1,100)
     # print(numero)
     print("Um número entre 1 a 100 foi sorteado! Adivinhe!")
-    while True:
-        
-        escolha = int(input("Escreva o número:"))   
-
-
     
+    while True:
+        try:
+            escolha = int(input("Escreva o número: "))
+        except ValueError:
+            print("Digite apenas números!")
+            continue
+            
         if escolha < 1 or escolha > 100:
-            print("Escolha um número entre 1 a 100!")
-
+            print("Escolha um número entre 1 a 100!")    
+        elif numero > escolha:
+            print("Seu número é menor que o número sorteado")
+        elif numero < escolha:
+            print("Seu número é maior que o número sorteado")
         else:
-            if numero > escolha:
-                print("Seu número é menor que o número sorteado")
-            elif numero < escolha:
-                print("Seu número é maior que o número sorteado")
-            else:
-                print("Você acertou!")
-                break
+            print("Você acertou!")
+            break
 
 game()
-
-
-# -----------------add def depois github-------------------
-
