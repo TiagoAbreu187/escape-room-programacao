e7e734a0d748ecb7a4358255082f81c517286474
Tiago Abreu <tiagoabreu.carv@gmail.com>
2026-06-21 14:46:21 -0300

    Update7 Jogo.py
    Multiplicador de dinheiro em base de tentativas

--- Jogo.py
+++ Jogo.py
@@ -4,11 +4,13 @@ import time
 
 def game():
     tentativas = 0
+    
     try:
+        reais = int(input("Quantos reais você quer apostar?\n->"))
         print("Sorteando multiplicador...")
         time.sleep(2)
         multiplicador = random.choices(range(1, 16),weights=[90, 40, 25, 15, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],k=1)[0]
-        print("Multiplicador Sorteado =",multiplicador)
+        print("Multiplicador Sorteado =",multiplicador,"\n")
         menu = int(input("Escolha uma dificuldade!\n1 para 100 números\n2 para 500 números\n3 para 1000 números\nEscreva aqui->"))
     except ValueError:
         print("Digite apenas números!")
@@ -44,13 +46,16 @@ def game():
         else:
             print("Você acertou em",tentativas,"tentativas!")
             if tentativas == 1:
+                print("Você ganhou",(reais*multiplicador)*100,"reais!")
                 winsound.Beep(1000, 150)
                 winsound.Beep(1500, 150)
                 winsound.Beep(2000, 150)
                 print("Você é muito sortudo!")
             elif tentativas >=2 and tentativas <=4:
+                print("Você ganhou",(reais*multiplicador)*4,"reais!")
                 print("Você é bom!")
             elif tentativas >=5 and tentativas <=9:
+                print("Você ganhou",(reais*multiplicador)*2,"reais!")
                 print("Tá na média, dá pra melhorar")
             else:
                 print("Você foi até o final!")
